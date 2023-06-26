c9063c96fd0959b1bb2e34e2c50fca325658451c
Jesselyn Hong <jesselyn.k.hong@gmail.com>
2023-06-26 10:33:23 -0700

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
